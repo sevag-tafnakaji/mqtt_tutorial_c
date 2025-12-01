c0af5f79d4da5a1296ad6c12d37cf14ab7b5dea3
Sevag Tafnakaji <sevag.tafnakaji@gmail.com>
2025-12-01 19:52:05 +0100

    Step 2.3: epoll functionality

--- src/network.h
+++ src/network.h
@@ -4,11 +4,14 @@
 #include <arpa/inet.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/epoll.h>
 #include <sys/types.h>
 
 // Socket families
 #define UNIX 0
 #define INET 1
+
+#define UUID_LEN 16
 
 /**
  * Set TCP_NODELAY flag to true, disable Nagle's alogrithm,
@@ -40,6 +43,105 @@ ssize_t send_bytes(int, const unsigned char *, size_t);
  * Receive (send) an arbitrary number of bytes from a file descriptor
  * and store them in a buffer
  */
-ssize_t recv_bytes(int, unsigned char *)
+ssize_t recv_bytes(int, unsigned char *, size_t);
+
+/**
+ * Event loop wrapper structure. Define an EPOLL loop and its status.
+ * The EPOLL instance use EPOLLONESHOT for each event and must be
+ * re-armed manually in order to allow future uses on a
+ * multi-threaded architecture
+ */
+struct evloop {
+  int epollfd;
+  int max_events;
+  int timeout;
+  int status;
+  struct epoll_event *events;
+  /* Dynamic array of periodic tasks, a pair descriptor - closure */
+  int periodic_maxsize;
+  int periodic_nr;
+  struct {
+    int timerfd;
+    struct closure *closure;
+  } **periodic_tasks;
+} evloop;
+
+typedef void callback(struct evloop *, void *);
+
+/**
+ * Callback object. Represents a callback function with an associated
+ * descriptor if needed. Args is a void pointer which can be a structure
+ * pointing to callback parameters and closure_id is an UUID for the
+ * closure itself.
+ * The last two fields are payload, a serialised version of the result of
+ * a callback, ready to be sent through wire, and a function pointer to
+ * the callback function to execute.
+ */
+struct closure {
+  int fd;
+  void *obj;
+  void *args;
+  char closure_id[UUID_LEN];
+  struct bytestring *payload;
+  callback *call;
+};
+
+struct evloop *evloop_create(int, int);
+void evloop_init(struct evloop *, int, int);
+void evloop_free(struct evloop *);
+
+/**
+ * Blocks in a while(1) loop awaiting for events to be raised on monitored
+ * file descriptors (sockets) and executing the paired callback previously
+ * registered
+ */
+int evloop_wait(struct evloop *);
+
+/**
+ * Register a closure with a function to be executed every time the paired
+ * descriptor is re-armed
+ */
+void evloop_add_callback(struct evloop *, struct closure *);
+
+/**
+ * Register a preiodic closure with a function to be executed every
+ * defined interval of time
+ */
+void evloop_add_periodic_task(struct evloop *, int, unsigned long long,
+                              struct closure *);
+
+/**
+ * Unregister a closure by removing the associated descriptor (socket) from
+ * the EPOLL loop
+ */
+int evloop_del_callback(struct evloop *, struct closure *);
+
+/**
+ * Re-arm the file descriptor (socket) associated with a closure for read
+ * action, restarting the event loop to monitor the callback for reading events
+ */
+int evloop_rearm_callback_read(struct evloop *, struct closure *);
+
+/**
+ * Re-arm the file descriptor (socket) associated with a closure for write
+ * action, restarting the event loop to monitor the callback for writing events
+ */
+int evloop_rearm_callback_write(struct evloop *, struct closure *);
+
+/* Epoll management functions */
+int epoll_add(int, int, int, void *);
+
+/**
+ * Modify an epoll-monitored descriptor, automatically set `EPOLLONESHOT` in
+ * addition to the other flags, which can be `EPOLLIN` for read and `EPOLLOUT`
+ * for write
+ */
+int epoll_mod(int, int, int, void *);
+
+/**
+ * Remove a descriptor from an epoll descriptor, making it no longer monitored
+ * for events.
+ */
+int epoll_del(int, int);
 
 #endif // NETWORK_H
